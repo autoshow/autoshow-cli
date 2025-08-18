e5f182063ba63f97d314f416609ecba19e9a79ae
Anthony Campolo <12433465+ajcwebdev@users.noreply.github.com>
2025-08-18 12:37:05 -0500

    remove music command

--- src/commander.ts
+++ src/commander.ts
@@ -6,7 +6,6 @@ import { createTextCommand } from './text/create-text-command.ts'
 import { createConfigCommand } from './config/create-config-command.ts'
 import { createMediaCommand } from './media/create-media-command.ts'
 import { createExtractCommand } from './extract/create-extract-command.ts'
-import { createMusicCommand } from './music/create-music-command'
 import { l, err } from '@/logging'
 import { argv, exit, fileURLToPath, basename } from '@/node-utils'
 
@@ -14,7 +13,7 @@ const program = new Command()
 
 program
   .name('autoshow-cli')
-  .description('Automate processing of audio/video content, manage meta-workflows, generate text-to-speech, create AI images, videos, and music, handle media operations, and extract text from PDFs.')
+  .description('Automate processing of audio/video content, manage meta-workflows, generate text-to-speech, create AI images, videos, handle media operations, and extract text from PDFs.')
   .version('1.0.0')
 
 const p = '[commander]'
@@ -23,7 +22,6 @@ program.addCommand(createTextCommand())
 program.addCommand(createTtsCommand())
 program.addCommand(createImageCommand())
 program.addCommand(createVideoCommand())
-program.addCommand(createMusicCommand())
 program.addCommand(createConfigCommand())
 program.addCommand(createMediaCommand())
 program.addCommand(createExtractCommand())
